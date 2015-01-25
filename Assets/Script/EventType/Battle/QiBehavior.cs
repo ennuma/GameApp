8daefe5d98ea5819c2f860534041b6393aa98ee0
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2015-01-24 21:56:00 -0800

    some other files added

---
+++ Assets/Script/EventType/Battle/QiBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QiBehavior : MonoBehaviour {
+	public GameObject StartPoint;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void createNewRythmObj(int num){
+		for (int i = 0; i < num; i++) {
+			Debug.Log ("dragon added");
+			GameObject dragon = NGUITools.AddChild (gameObject, Resources.Load ("Dragon") as GameObject);
+			dragon.transform.position = StartPoint.transform.position;
+		}
+		//Invoke("createNewRythmObj",5); // 五秒建造一个rythmobj
+	}
+}
